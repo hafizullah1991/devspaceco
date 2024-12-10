f4cd09ad6f5ce61c239ba6473f160611bd4fac74
hafizullah1991 <Ahmadshakhi2020@gmail.com>
2024-12-10 10:18:46 -0800

    logic for login added

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormsModule, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'dc-login',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class Login {
-
+export class Login implements OnInit {
+  public contactLogInForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl('')
+  })
+ngOnInit(): void {
+  
+}
+public submitForm(): void{
+  console.log( )
+}
 }
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormsModule, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'dc-signup',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, ReactiveFormsModule ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
-export class Signup {
+export class Signup implements OnInit{
+public contactSignupForm = new FormGroup({
+  name: new FormControl(''),
+  email: new FormControl(''),
+  password: new FormControl(''),
+  repeatpassword: new FormControl('')
+});
+ngOnInit(): void {}
+  public submitForm(): void{
+console.log(this.contactSignupForm.value)
+  }
 
 }
